77265937c2c43a2a43a7e2e4f91db189fb4ca142
Sander De Pauw <sander.de.pauw@student.ehb.be>
2022-11-30 20:51:36 +0100

    Added player hitbox and groundsensor Added coin hitboxes and corrected view to align with hitbox

--- Short_platformer_game/src/main/java/org/platformer/PlatformerFactory.java
+++ Short_platformer_game/src/main/java/org/platformer/PlatformerFactory.java
@@ -2,13 +2,18 @@ package org.platformer;
 
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.*;
+import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.entity.components.IrremovableComponent;
 import com.almasb.fxgl.physics.BoundingShape;
 import com.almasb.fxgl.physics.HitBox;
 import com.almasb.fxgl.physics.PhysicsComponent;
 import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
+import com.almasb.fxgl.physics.box2d.dynamics.FixtureDef;
+import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
+
+import static com.almasb.fxgl.dsl.FXGL.entityBuilder;
 
 public class PlatformerFactory implements EntityFactory {
 
@@ -26,19 +31,32 @@ public class PlatformerFactory implements EntityFactory {
         PhysicsComponent physics = new PhysicsComponent();
         physics.setBodyType(BodyType.DYNAMIC);
 
-        return FXGL.entityBuilder(data)
+        //groundsensor detects if playercharacter is on the ground
+        physics.addGroundSensor(new HitBox("GROUND_SENSOR", new Point2D(5, 30), BoundingShape.box(23, 5)));
+
+        // this avoids player sticking to walls
+        physics.setFixtureDef(new FixtureDef().friction(0.0f));
+
+        return entityBuilder(data)
                 .type(EntityType.PLAYER)
-                .viewWithBBox(new Rectangle(30, 30, Color.BLUE))
-                .with(new PhysicsComponent())
+                .bbox(new HitBox(new Point2D(5,5), BoundingShape.box(23, 25)))
+                .with(physics)
+                .with(new CollidableComponent(true))
+                .with(new IrremovableComponent())
+                .with(new PlayerComponent())
                 .build();
     }
 
     @Spawns("coin")
     public Entity newCoin(SpawnData data) {
+        Circle view = new Circle(data.<Integer>get("width") / 2, Color.GOLD);
+        view.setCenterX(8);
+        view.setCenterY(-8);
         return FXGL.entityBuilder(data)
                 .type(EntityType.COIN)
-                .viewWithBBox(new Circle(data.<Integer>get("width") / 2, Color.GOLD))
-                .with(new PhysicsComponent())
+                .bbox(new HitBox(new Point2D(0, -16), BoundingShape.circle(data.<Integer>get("width") / 2)))
+                .view(view)
+                .with(new CollidableComponent(true))
                 .build();
     }
 }
