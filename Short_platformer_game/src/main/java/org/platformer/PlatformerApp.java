2521e333bcf9df5ad08fcc282ad5cbf3d9dc7106
Sander De Pauw <sander.de.pauw@student.ehb.be>
2022-11-30 18:33:52 +0100

    Changes made to code to generate first map correctly

--- Short_platformer_game/src/main/java/org/platformer/PlatformerApp.java
+++ Short_platformer_game/src/main/java/org/platformer/PlatformerApp.java
@@ -1,7 +1,10 @@
 package org.platformer;
 
+import com.almasb.fxgl.app.ApplicationMode;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.app.scene.LoadingScene;
+import com.almasb.fxgl.app.scene.SceneFactory;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.level.Level;
@@ -10,14 +13,16 @@ public class PlatformerApp extends GameApplication {
 
     @Override
     protected void initSettings(GameSettings settings) {
-        settings.setWidth(15 * 70);
-        settings.setHeight(10 * 70);
-        settings.setTitle("Basic Game App");
+        settings.setWidth(50 * 16);
+        settings.setHeight(38 * 16);
+
+        settings.setApplicationMode(ApplicationMode.DEVELOPER);
     }
 
     @Override
     protected void initGame() {
-        FXGL.getGameWorld().setLevel(FXGL.setLevelFromMap("tmx/platformer.tmx"));
+        FXGL.getGameWorld().addEntityFactory(new PlatformerFactory());
+        FXGL.setLevelFromMap("tmx/platformer.tmx");
 
     }
 
--- Short_platformer_game/src/main/java/org/platformer/PlatformerFactory.java
+++ Short_platformer_game/src/main/java/org/platformer/PlatformerFactory.java
@@ -7,21 +7,31 @@ import com.almasb.fxgl.physics.HitBox;
 import com.almasb.fxgl.physics.PhysicsComponent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 public class PlatformerFactory implements EntityFactory {
 
     @Spawns("platform")
     public Entity newPlatform(SpawnData data) {
-        return FXGL.entityBuilder()
+        return FXGL.entityBuilder(data)
                 .type(EntityType.PLATFORM)
                 .bbox(new HitBox(BoundingShape.box(data.<Integer>get("width"), data.<Integer>get("height"))))
                 .with(new PhysicsComponent())
                 .build();
     }
 
+    @Spawns("player")
+    public Entity newPlayer(SpawnData data) {
+        return FXGL.entityBuilder(data)
+                .type(EntityType.PLAYER)
+                .viewWithBBox(new Rectangle(30, 30, Color.BLUE))
+                .with(new PhysicsComponent())
+                .build();
+    }
+
     @Spawns("coin")
     public Entity newCoin(SpawnData data) {
-        return FXGL.entityBuilder()
+        return FXGL.entityBuilder(data)
                 .type(EntityType.COIN)
                 .viewWithBBox(new Circle(data.<Integer>get("width") / 2, Color.GOLD))
                 .with(new PhysicsComponent())
